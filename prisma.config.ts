661d3788036a84e887b3be11790a7366984195b7
Rezapu Obed <obed.ubaidillah@gmail.com>
2026-01-03 09:58:01 +0700

    fix: prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,12 @@
-import { defineConfig } from '@prisma/config';
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-export default defineConfig({});
+export default defineConfig({
+    schema: "prisma/schema.prisma",
+    migrations: {
+        path: "prisma/migrations",
+    },
+    datasource: {
+        url: process.env["DATABASE_URL"],
+    },
+});
